5fffc14e23a65e728d2cd192e222a39f7e3b2251
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-13 19:57:33 +0000

    Creating branch for BioC 3.2 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/ChemmineR@109591 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/chemmine_tools.R
@@ -0,0 +1,155 @@
+# Purpose: R interface to ChemMine Tools
+# by Tyler William H Backman
+
+.serverURL <- "http://chemmine.ucr.edu/ChemmineR/"
+
+# check job status
+status <- function(object){
+    if(class(object) != "jobToken"){
+        stop("input not of class jobToken")
+    }
+    response <- postForm(paste(.serverURL, "jobStatus", sep=""), task_id=slot(object, "jobId"))[[1]]
+    if(grepl("^ERROR:", response)){
+        stop(response)
+    }
+    return(response)
+}
+
+# browse job online (works only once, saves to user account)
+browseJob <- function(object){
+    if(class(object) != "jobToken"){
+        stop("input not of class jobToken")
+    }
+    url <- paste(.serverURL, "showJob", "/", slot(object, "jobId"), sep="")
+    browseURL(url)
+    return(url)
+}
+
+# get result 
+result <- function(object){
+    if(class(object) != "jobToken"){
+        stop("input not of class jobToken")
+    }
+    response <- "RUNNING"
+    while(response == "RUNNING"){
+        Sys.sleep(2)
+        response <- postForm(paste(.serverURL, "jobResult", sep=""), task_id=slot(object, "jobId"))[[1]]
+    }
+    if(grepl("^ERROR:", response)){
+        stop(response)
+    }
+    if(response == "FAILED"){
+        stop("Job Failed")
+    }
+    response <- .convertOutput(response, slot(object, "tool_name"))
+    return(response)
+}
+
+# Purpose: retrieve list of all tools from server
+listCMTools <- function(){
+    response <- postForm(paste(.serverURL, "listCMTools", sep=""), category="all")[[1]]
+    if(grepl("^ERROR:", response)){
+        stop(response)
+    }
+    read.table(text=response, sep="\t", header=T)
+}
+
+# Purpose: retrieve details on a tool 
+toolDetails <- function(tool_name){
+    response <- postForm(paste(.serverURL, "toolDetails", sep=""), tool_name=tool_name)[[1]]
+    if(grepl("^ERROR:", response)){
+        stop(response)
+    }
+    cat(response)
+}
+
+# Purpose: launch a ChemMine Tools job on server
+launchCMTool <- function(tool_name, input = "", ...){
+    toolList <- listCMTools()
+    if(! tool_name %in% toolList$Name){
+        stop("invalid tool name")
+    }
+    input <- .convertInput(input, tool_name)
+    response <- postForm(paste(.serverURL, "launchCMTool", sep=""), tool_name = tool_name, input = input, ...)[[1]]
+    if(grepl("^ERROR:", response)){
+        stop(response)
+    }
+    new("jobToken",
+        tool_name = tool_name,
+        jobId = response
+    )
+}
+
+# Purpose: convert input to correct format
+.convertInput <- function(input, toolName){
+    response <- postForm(paste(.serverURL, "getConverter", sep=""), converterType="input", toolName=toolName)[[1]]
+    objectClass <- gsub("\n.*", "", response)
+    converter <- gsub("^.*?\n", "", response)
+
+    if(objectClass == "data.frame"){
+        try(input <- as.data.frame(input), silent = TRUE)
+    } else {
+        try(input <- as(input, objectClass), silent = TRUE)
+    }
+
+    if(class(input) != objectClass){
+        stop(paste("input not of class", objectClass))
+    }
+
+    eval(parse(text = converter))
+}
+
+# Purpose: convert output to correct format 
+.convertOutput <- function(output, toolName){
+    response <- postForm(paste(.serverURL, "getConverter", sep=""), converterType="output", toolName=toolName)[[1]]
+    converter <- gsub("^.*?\n", "", response)
+
+    eval(parse(text = converter))
+}
+
+##################################
+# Wrappers for old web functions #
+##################################
+
+# view sdfs in ChemMine Tools
+sdf.visualize <- function(sdf){
+    if(! class(sdf) == "SDFset"){
+        stop('input not of class \"SDFset\"')
+    } 
+    job <- launchCMTool("sdf.visualize", sdf)
+    browseJob(job)
+}
+
+# get CIDs from PubChem through ChemMine Web Tools
+getIds <- function(cids) {
+    if(! class(cids) == "numeric"){
+        stop('reference compound ids must be of class \"numeric\"')
+    }
+    if(length(cids) == 0){
+        stop('no compounds to retrieve- input must contain at least one cid')
+    }
+    jobToken <- launchCMTool("pubchemID2SDF", cids)
+    result(jobToken)
+}
+
+# search PubChem through ChemMine Web Tools with smiles query
+searchString <- function(smiles) {
+    if(class(smiles) == "SMIset")
+        smiles = as.character(smiles)
+    if(! class(smiles) == "character"){
+        stop('reference compound must be a smiles string of class \"character\"')
+    } 	
+    sdfquery <- smiles2sdf(smiles)
+    searchSim(sdfquery)
+}
+
+# search PubChem through ChemMine Web Tools with sdf query
+searchSim <- function(sdf) {
+    if(! class(sdf) == "SDFset"){
+        stop('reference compound must be a compound of class \"SDFset\"')
+    } 
+    jobToken <- launchCMTool('Fingerprint Search', sdf, 'Similarity Cutoff'=0.9, 'Max Compounds Returned'=200)
+    cids <- as.numeric(result(jobToken))
+    if(length(cids) == 0) return(SDFset())
+    getIds(cids)
+}
---
+++ inst/unitTests/test_general.R
@@ -0,0 +1,196 @@
+
+
+
+test.formatConversions <- function() {
+	message("test.formatConversions")
+
+	data(sdfsample)
+
+	smiles = sdf2smiles(sdfsample[1:10])
+	sdfs = smiles2sdf(smiles)
+	smiles2 = sdf2smiles(sdfs)
+
+	checkEquals(smiles,smiles2)
+
+}
+
+test.genAPDescriptors <- function(){
+
+	DEACTIVATED("removed old version of function")
+
+	data(sdfsample)
+	
+	for(i in 1:100){
+		sdf = sdfsample[[i]]
+		desc = genAPDescriptors(sdf) 
+		#print(head(desc));
+
+		oldDesc=ChemmineR:::.gen_atom_pair(ChemmineR:::SDF2apcmp(sdf))
+                 
+		#print(oldDesc);
+		compResult = desc==oldDesc
+		if(!all(compResult)){
+			message("descriptor mismatch")
+			print(oldDesc[!compResult])
+			print(desc[!compResult])
+			message("----------")
+			firstFalse = match(FALSE,compResult)
+			print(oldDesc[(firstFalse-5):(firstFalse+5)])
+			print(desc[(firstFalse-5):(firstFalse+5)])
+
+		}
+		checkTrue(all(compResult))
+		#checkEqualsNumeric(desc,oldDesc)
+	}
+}
+test.propOB <- function() {
+	data(sdfsample)
+	p = propOB(sdfsample[1:5])
+	#print(p)
+	#checkEquals(ncol(p),15)
+	checkEquals(nrow(p),5)
+   checkEquals(p$MW[2],MW(sdfsample[2])[[1]])
+
+}
+test.fingerprintOB <- function(){
+	if(require(ChemmineOB)){
+		data(sdfsample)
+		fp = fingerprintOB(sdfsample[1:5],"FP2")
+		checkEquals(fptype(fp),"FP2")
+		fpSingle = fingerprintOB(sdfsample[1],"FP2")
+		checkEquals(as.character(class(fpSingle)),"FPset")
+		checkEqualsNumeric(as.matrix(fpSingle[1]), as.matrix(fp[1]))
+	}
+}
+test.obmolRefs <- function() {
+	data(sdfsample)
+	if(require(ChemmineOB)){
+		obmolRef = obmol(sdfsample[[1]])
+		checkEquals(class(obmolRef),"_p_OpenBabel__OBMol")
+
+		obmolRefs = obmol(sdfsample)
+		checkEquals(class(obmolRefs),"list")
+		checkEquals(class(obmolRefs[[2]]),"_p_OpenBabel__OBMol")
+		checkEquals(length(sdfsample),length(obmolRefs))
+	}else
+		checkException(obmol(sdfsample[[1]]))
+
+}
+test.smartsSearchOB <- function(){
+	data(sdfsample)
+	rotableBonds = smartsSearchOB(sdfsample[1:5],"[!$(*#*)&!D1]-!@[!$(*#*)&!D1]",uniqueMatches=FALSE)
+	print("rotable bonds: ")
+	print(rotableBonds)
+	print(sdfid(sdfsample[1:5]))
+	checkEquals(as.vector(rotableBonds[1:5]),c(24,20,14,30,10))
+
+}
+test.fpSim <- function(){
+	data(apset)
+	fpset = desc2fp(apset)
+	dists = fpSim(fpset[[1]],fpset,top=6)
+	checkEqualsNumeric(dists, 
+							 c(1.0000000,0.4719101,0.4288499,0.4275229,0.4247423,0.4187380),
+							 tolerance = 0.0001)
+
+	for(m in c("tanimoto","euclidean","tversky","dice")){
+		sim = ChemmineR:::fpSimOrig(fpset[[1]],fpset,
+					method=m,cutoff=0.4,top=6)
+		simFast= fpSim(fpset[[1]],fpset,
+					method=m,cutoff=0.4,top=6)
+		#message("method: ",m)
+		#print(sim)
+		#print(simFast)
+		checkEqualsNumeric(sim,simFast,tolerance=0.00001)
+	}
+
+	
+}
+test.fpSimParameters<- function(){
+	data(apset)
+	fpset = desc2fp(apset)
+
+	params = genParameters(fpset)
+
+	similarities = fpSim(fpset[[1]],fpset,top=6,parameters=params)
+	#print(similarities)
+
+	checkEqualsNumeric(similarities$similarity, 
+							 c(1,0.471910112359551,0.428849902534113,0.427522935779817,0.424742268041237,0.418738049713193),
+							 tolerance = 0.0001)
+	checkEqualsNumeric(similarities$zscore, 
+							 c(6.41202501933441,1.6379277432492,1.24865001753133,1.23665382424036,1.21151572069559,1.15723571451518),
+							 tolerance = 0.0001)
+	checkEqualsNumeric(similarities$evalue, 
+							 c(0.00000,6.36243,11.64270,11.84515,12.27769,13.25050),
+							 tolerance = 0.0001)
+	checkEqualsNumeric(similarities$pvalue, 
+							 c(0,0.998274830604939,0.999991217102066,0.999992826756578,0.999995345559029,0.999998240535916),
+							 tolerance = 0.0001)
+
+}
+test.exactMassOB <- function(){
+	data(sdfsample)
+	mass = exactMassOB(sdfsample[1:5])
+	checkEqualsNumeric(mass,c(456.2009,357.1801,
+									  370.1100,461.1733,
+									  318.1943),tolerance=0.00001)
+}
+test.3dCoords <-function(){
+	DEACTIVATED("causing timeout on bioc, disabling for now")
+	data(sdfsample)
+	sdf3d = generate3DCoords(sdfsample[1])
+
+	checkTrue(!any(atomblock(sdf3d)[[1]][,3]==0))
+	
+}
+test.canonicalize <- function(){
+	data(sdfsample)
+	cansdf = canonicalize(sdfsample[1])
+
+	bb=bondblock(cansdf)[[1]]
+
+	checkEqualsNumeric(bb[1,1:3],c(2,3,1))
+	checkEqualsNumeric(bb[2,1:3],c(2,4,1))
+
+}
+test.parseV3000 <- function() {
+
+	DEACTIVATED("requires local files")
+	sdfset2 = read.SDFset("~/runs/v3000/DrugLike-0_2-3K3K_1.v2k.sdf")
+	sdfset3 = read.SDFset("~/runs/v3000/DrugLike-0_2-3K3K_1.sdf")  
+
+	compareSdfVersions = function(v2k,v3k){
+		checkEquals(sdfid(v2k),sdfid(v3k))
+
+		#message("v2k: ",nrow(atomblock(v2k)),"x",ncol(atomblock(v2k)))
+		#message("v3k: ",nrow(atomblock(v3k)),"x",ncol(atomblock(v3k)))
+		#print(head(atomblock(v2k)))
+		#print(head(atomblock(v3k)))
+		toCompare = c(1:5,7:10) #exclude colum 6
+		checkTrue( all(atomblock(v2k)[,toCompare] ==
+							atomblock(v3k)[,toCompare]))
+
+#		cmp = bondblock(v2k)[,1:3] == bondblock(v3k)
+#		if(! all(cmp)){
+#			mismatched = which(cmp==FALSE)
+#			print(cmp)
+#			print("mismatched: ")
+#			print(mismatched)
+#			print("data:")
+#			print(bondblock(v2k)[mismatched,1:3])
+#			print(bondblock(v3k)[mismatched,])
+#		}
+#		checkTrue( all(bondblock(v2k)[,1:3] == bondblock(v3k)))
+
+		checkTrue( all(datablock(v2k) == datablock(v3k)))
+	}
+	for(i in seq(along=sdfset2)){
+		#if(!(i  %in% c(38,39,89))){ # this differ in acceptable ways
+		#	message("testing ",i, " id: ",sdfid(sdfset2[i]))
+			compareSdfVersions(sdfset2[[i]],sdfset3[[i]])
+		#}
+	}
+
+
+}
